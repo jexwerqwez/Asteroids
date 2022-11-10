c26edbf88df2063dbe03a49f2b2669022a913733
jexwerqwez quez <spacevoid.99@gmail.com>
2022-11-10 20:48:03 +0000

    add include

---
+++ bonus.h
@@ -0,0 +1,10 @@
+#ifndef BONUS
+#define BONUS
+#include "base.h"
+#include "objects.h"
+#include "field.h"
+#include "asteroids.h"
+#include "spaceship.h"
+#include "gun.h"
+#include "game.h"
+#endif
