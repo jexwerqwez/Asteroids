560cfecc20c21b20cc608cf777c3edcd58e53caa
jexwerqwez quez <spacevoid.99@gmail.com>
2022-11-10 20:49:44 +0000

    add include

---
+++ objects.h
@@ -0,0 +1,23 @@
+#ifndef OBJECTS
+#define OBJECTS
+#include "base.h"
+class Space_Object {
+    int x;
+    int y;
+    public:
+        Space_Object(int x_ = 0, int y_ = 0): x(x_), y(y_) {};
+        int getX() {return x;};
+        int getY() {return y;};
+        void setX(int xx) {x = xx;};
+        void setY(int yy) {y = yy;};
+        int operator==(Space_Object pos) {
+            return this->x == pos.getX() && this->y == pos.getY();
+        }
+        Space_Object operator+(Space_Object pos) {
+            return Space_Object(this->x + pos.getX(), this->y + pos.getY());
+        }
+        Space_Object operator-(Space_Object pos) {
+            return Space_Object(this->x - pos.getX(), this->y - pos.getY());
+        }
+};
+#endif
