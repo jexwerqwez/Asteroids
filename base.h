1090e30f8300a95a42e15c322325438aaad3e264
jexwerqwez quez <spacevoid.99@gmail.com>
2022-11-10 20:47:40 +0000

    add include

---
+++ base.h
@@ -0,0 +1,12 @@
+#ifndef BASE
+#define BASE
+#include <iostream>
+#include <ncursesw/ncurses.h>
+#include <ctime>
+#include <sys/time.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <unistd.h>
+using namespace std;
+#endif
