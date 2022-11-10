78e3700cada64b7eac3ebd62c846f567e1f1995f
jexwerqwez quez <spacevoid.99@gmail.com>
2022-11-10 20:50:35 +0000

    add include

---
+++ spaceship.h
@@ -0,0 +1,23 @@
+#ifndef SPACESHIP
+#define SPACESHIP
+#include "base.h"
+#include "objects.h"
+#include "field.h"
+class Spaceship: public Space_Object{
+        int health;
+        char spaceship;
+    protected:
+        Space_Object position;
+    public:
+        Spaceship(int h, char ssp, Space_Object pos): health(h), spaceship(ssp), position(pos) {};
+        void draw_spaceship();
+        void erase_spaceship();
+        int getHealt() {return health;};
+        void setHeath(int h) {health = h;};
+        char getSprite() {return spaceship;};
+        void moveHorizontal(int);
+        void moveVertical(int);
+        Space_Object change_position(int, Field);
+        Space_Object getPos() {return position;};
+};
+#endif
