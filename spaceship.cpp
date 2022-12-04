498e1842d6eeb95f3cd74b901b1fc9dd8a51367e
jexwerquez <119809115+jexwerquez@users.noreply.github.com>
2022-12-05 00:32:36 +0300

    Update spaceship.cpp

--- spaceship.cpp
+++ spaceship.cpp
@@ -10,7 +10,7 @@ void Spaceship::moveVertical(int y) {
 
 void Spaceship::draw_spaceship() {
     move(position.getY(), position.getX());
-    mvaddch(position.getY(), position.getX(), getSprite() | COLOR_PAIR(3));
+    mvaddch(position.getY(), position.getX(), getSprite() | COLOR_PAIR( (getHealt() > 3) ? 5 : 4));
 }
 
 void Spaceship::erase_spaceship() {
@@ -18,27 +18,27 @@ void Spaceship::erase_spaceship() {
 }
 
 Space_Object Spaceship::change_position(int command, Field field) {
-        switch (command) {
-            case 'd': {
-                if (field.object_inside(getPos()) && (getPos().getX() < field.getFieldWidth() - 2))
-                    moveHorizontal(1);
-                return getPos();
-            }
-            case 'a': {
-                if (field.object_inside(getPos()) && (getPos().getX() > 1))
-                    moveHorizontal(-1);
-                return getPos();
-            }
-            case 'w': {
-                if (field.object_inside(getPos()) && (getPos().getY() > 1))
-                    moveVertical(-1);
-                return getPos();
-            }
-            case 's': {
-                if (field.object_inside(getPos()) && (getPos().getY() < field.getFieldHeight()- 2))
-                    moveVertical(1);
-                return getPos();
-            }
+    switch (command) {
+        case 'd': {
+            if (field.object_inside(getPos()) && (getPos().getX() < field.getFieldWidth() - 2))
+                moveHorizontal(1);
+            return getPos();
         }
+        case 'a': {
+            if (field.object_inside(getPos()) && (getPos().getX() > 1))
+                moveHorizontal(-1);
+            return getPos();
+        }
+        case 'w': {
+            if (field.object_inside(getPos()) && (getPos().getY() > 1))
+                moveVertical(-1);
+            return getPos();
+        }
+        case 's': {
+            if (field.object_inside(getPos()) && (getPos().getY() < field.getFieldHeight()- 2))
+                moveVertical(1);
+            return getPos();
+        }
+    }
     return getPos();
 }
