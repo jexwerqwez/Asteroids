07dd69b7530c3ed980216160e4a4abc64fae6aea
jexwerquez <119809115+jexwerquez@users.noreply.github.com>
2022-12-05 14:06:19 +0300

    new draw field

--- field.cpp
+++ field.cpp
@@ -5,24 +5,25 @@ bool Field::object_inside(Space_Object object) {
             object.getY() > 0 && object.getY() < height-1;
 }
 
-void Field::draw_field() {
+void Field::draw_field(int mode) {
     move(0,0);
+    short int bord_color = 0;
+    short int field_color = 0;
+    char cell_type = ' ';
     for(int i = 0; i < height; i++) {
-        addch(' ' | COLOR_PAIR(8));
-        addch('\n');
-        //printw("#\n");
-    }
-    move(0, 1);
-    for(int i = 0; i < width; i++) {
-        addch(' ' | COLOR_PAIR(8));
-    }
-    move(height-1, 1);
-    for(int i = 0; i < width; i++) {
-        addch(' ' | COLOR_PAIR(8));
-    }
-    for(int i = 1; i < height; i++) {
-        move(i, width);
-        addch(' ' | COLOR_PAIR(8));
+        for(int j = 0; j < width+1; j++) {
+            if (mode == 0) {
+                bord_color = 8;
+                field_color = 3;
+                cell_type = ' ';
+            } else if (mode == 1) {
+                bord_color = 8;
+                field_color = 9;
+                cell_type = '`';
+            }
+            if(i == 0 || j == 0 || i == height-1 || j == width) mvaddch(i,j,cell_type | COLOR_PAIR(bord_color));
+            else mvaddch(i,j,cell_type | COLOR_PAIR(field_color));
+        }
     }
     move(height+2, width/2-5);
 }
