0b5dcd5af76e1994065ffd8ae002753d069c8381
jexwerquez <119809115+jexwerquez@users.noreply.github.com>
2022-12-07 00:33:25 +0300

    codestyle

--- field.h
+++ field.h
@@ -3,13 +3,21 @@
 #include "base.h"
 #include "objects.h"
 class Field {
-    int height;
-    int width;
-    public:
-        Field(int h, int w): height(h), width(w) {};
-        int getFieldWidth() {return width;};
-        int getFieldHeight() {return height;};
-        bool object_inside(Space_Object object);
-        void draw_field(int mode);
+  int height;
+  int width;
+  int newheight = height;
+  int newwidth = width;
+
+ public:
+  Field(int h, int w) : height(h), width(w){};
+  int getFieldWidth() { return width; };
+  void setFieldWidth(int w) { newwidth = w; };
+  int getNewFieldWidth() { return newwidth; };
+  int getFieldHeight() { return height; };
+  void setFieldHeight(int h) { newheight = h; };
+  int getNewFieldHeight() { return newheight; };
+  bool object_inside(Space_Object object);
+  void draw_field(int mode);
+  void draw_circle(int d);
 };
 #endif
