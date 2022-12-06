39ca6409ab57448df8580ed3fc31a15942c706e7
jexwerquez <119809115+jexwerquez@users.noreply.github.com>
2022-12-07 00:37:25 +0300

    codestyle

--- settings.h
+++ settings.h
@@ -2,12 +2,12 @@
 #define SETTINGS
 #include "base.h"
 struct Settings {
-    unsigned height;
-    unsigned width;
-    int score;
-    unsigned hithpoint;
-    Settings(unsigned h = 10, unsigned w = 10, int s = 0, unsigned hp = 1): height(h), 
-            width(w), score(s), hithpoint(hp) {};
-    int parser(Settings &set, string &filename);
+  unsigned height;
+  unsigned width;
+  int score;
+  unsigned hithpoint;
+  Settings(unsigned h = 10, unsigned w = 10, int s = 0, unsigned hp = 1)
+      : height(h), width(w), score(s), hithpoint(hp){};
+  int parser(Settings &set, string &filename);
 };
 #endif
