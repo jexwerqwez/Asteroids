e4abc8dedf76aee1bc9db8aabeb265e19fe6e4fd
jexwerquez <119809115+jexwerquez@users.noreply.github.com>
2022-12-07 00:35:11 +0300

    codestyle

--- gun.h
+++ gun.h
@@ -1,31 +1,34 @@
 #ifndef GUN
 #define GUN
 #include "base.h"
-#include "objects.h"
 #include "field.h"
+#include "objects.h"
 #include "spaceship.h"
-class Shot: public Space_Object {
-        char shot;
-    protected:
-        Space_Object position;
-    public:
-        Shot(char shsp, Space_Object pos): shot(shsp), position(pos) {};
-        void draw_shot();
-        void erase_shot();
-        void moveShot(int);
-        void makeShot(Space_Object);
-        char getSprite() {return shot;};
-        Space_Object getPos() {return position;};
+class Shot : public Space_Object {
+  char shot;
+
+ protected:
+  Space_Object position;
+
+ public:
+  Shot(char shsp, Space_Object pos) : shot(shsp), position(pos){};
+  void draw_shot();
+  void erase_shot();
+  void moveShot(int);
+  void makeShot(Space_Object);
+  char getSprite() { return shot; };
+  Space_Object getPos() { return position; };
 };
 class Gun {
-    Field field;
-    vector <Shot*> shots;
-    Spaceship* spaceship;
-    public:
-        Gun(Field f): field(f) {};
-        vector<Shot*> getShots() {return shots;};
-        Field getField() {return field;};
-        void destruct_shot(int);
-        int gun_manager(Space_Object, int, int);
+  Field field;
+  vector<Shot*> shots;
+  Spaceship* spaceship;
+
+ public:
+  Gun(Field f) : field(f){};
+  vector<Shot*> getShots() { return shots; };
+  Field getField() { return field; };
+  void destruct_shot(int);
+  int gun_manager(Space_Object, int, int);
 };
 #endif
